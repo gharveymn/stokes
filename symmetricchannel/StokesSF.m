5f79551f179799e599a8dd4ad5516b06509c6d70
Gene Harvey <gharveymn@gmail.com>
2017-05-22 14:57:11 -0500

    completes spectral decomp fomulation

--- symmetricchannel/StokesSF.m
+++ symmetricchannel/StokesSF.m
@@ -24,11 +24,12 @@ function StokesSF
 	bcinds = rhs;
 	
 	%inflow
-	
+	outwidth = ymax-ymin;
 	inflowx = xmin*ones(numel(xmesh),1);
 	for i=1:numel(yinit)
 		ind = (xmesh==inflowx+h & ymesh==yinit(i));
-		in(ind) = yinit(i)./4 - yinit(i).^3./3 + 1/12;
+		%in(ind) = yinit(i)./4 - yinit(i).^3./3 + 1/12;
+		in(ind) = 1/outwidth^3*(outwidth^2/4*yinit(i) - yinit(i)^3/3) + 1/12;
 		bcinds = bcinds | ind;
 	end
 	
@@ -66,106 +67,19 @@ function StokesSF
 	xsz = numel(xinit);
 	ysz = numel(yinit);
 	
-	
-	rmesh = filterMat'*rhs;
-	Rmesh = reshape(rmesh,[xsz,ysz])';
-	
-	figure(1)
-	ax = MakeAxis(Xmesh,Ymesh);
-	surf(Xmesh,Ymesh,Rmesh,'edgecolor','none','facecolor','interp')
-	axis(ax)
-	
 	%make derivative matrices
-	bih = biharmonic2(xsz,ysz,h);
 	
-	bcw = 0&on;
-	bce = 0&on;
-	bcs = 0&on;
-	bcn = 0&on;
-	bcc = 0&on;
+	%change when switch to rectangular
+	A = 1/h^2*sptoeplitz([2 -1],xsz);
+	C = sparse([1,xsz],[1,xsz],[(2/h^4),(2/h^4)],xsz,xsz);
 	
-	%impose Neumann conditions -- along all boundaries
-	%unfortunately I think we need to use loops for this
+	[P,D] = eigs(C,A,xsz);
 	
-% 	%TODO: think about corners
-% 	for i=1:numel(on)
-% 		if(on(i))
-% 			w = xmeshfull(i)==xmin;
-% 			e = xmeshfull(i)==xmax;
-% 			s = ymeshfull(i)==ymin;
-% 			n = ymeshfull(i)==ymax;
-% 			
-% 			if(~w)
-% 				w = ~valind(i-1);
-% 			end
-% 			
-% 			if(~e)
-% 				e = ~valind(i+1);
-% 			end
-% 			
-% 			if(~s)
-% 				s = ~valind(i-xsz);
-% 			end
-% 			
-% 			if(~n)
-% 				n = ~valind(i+xsz);
-% 			end
-% 			
-% 			%if two of these are satisfied then we have a corner
-% 			%cannot be three since Lipschitz, cannot be w&e or n&s for the same reason
-% 			%if all false then its on surrounded by all but one side (since it's on the boundary), so corner
-% 			
-% 			if((w&&n)||(w&&s)||(e&&n)||(e&&s)||~(w||e||s||n))
-% 				%corner boundary
-% 				bcc(i) = 1;
-% 			end
-% 			%west boundary
-% 			bcw(i) = w;
-% 			%east boundary
-% 			bce(i) = e;
-% 			%south boundary
-% 			bcs(i) = s;
-% 			%north boundary
-% 			bcn(i) = n;
-% 		end
-% 	end
+	G = P'*reshape(filterMat'*rhs,xsz,xsz)*P;
+	V = G./((4+repmat(diag(D),1,xsz)+repmat(diag(D)',xsz,1)));
+	psi = reshape(P*V*P',xsz*ysz,1);
 	
-	bcsz = numel(bcw);
-	
-	%wipe out invalid indices
-	bih = filterMat*bih*filterMat';
-	bcw = bcw(valind);
-	bce = bce(valind);
-	bcs = bcs(valind);
-	bcn = bcn(valind);
-	
-	sz = size(bih,1);
-	
-	%impose Dirichlet conditions
-	%we do this by just wiping out the row by row multiplication and adding back a diagonal of ones
-	bih = ~bcinds.*bih + spdiags(bcinds,0,sz,sz);
-%	bih = ~(bcw&bce&bcn&bcs).*bih + spdiags(bcw&bce&bcs&bcn,0,sz,sz);
-	
-	
-	bih = ~onpf.*bih + spdiags(onpf,0,sz,sz);
-	
-	cond(full(bih))
-	
-	psi = bih\rhs;
-	
-	on = on|circshift(on,-1)|circshift(on,1)|circshift(on,-xsz)|circshift(on,xsz);
-	
-	
-	valindinner = valind&~on;
-	filterMatinner = spdiags(valindinner,0,xsz*ysz,xsz*ysz);
-     filterMatinner = filterMatinner(valindinner,:);
-	
-	Xmesh = (reshape(xmeshfull./valindinner,[xsz,ysz]))';
-     Ymesh = (reshape(ymeshfull./valindinner,[ysz,xsz]))';
-
-     xmesh = filterMatinner*xmeshfull;
-     ymesh = filterMatinner*ymeshfull;
-	
+	psi = filterMat*psi;
 	
 	%make some derivative operator matrices
 	%TODO: just make these into a function in the path
@@ -174,28 +88,33 @@ function StokesSF
 	Dx(1,:) = 0;
 	Dx(end,:) = 0;
 	dx = kron(speye(ysz),Dx);
-	dx = filterMatinner*dx*filterMatinner';
+	dx = filterMat*dx*filterMat';
 	dx = dx.*~(sum(dx,2)~=0);
 	
 	Dy = sptoeplitz([0 -1],[0 1],ysz)./(2*h);
 	Dy(1,:) = 0;
 	Dy(end,:) = 0;
 	dy = kron(Dy,speye(xsz));
-	dy = filterMatinner*dy*filterMatinner';
+	dy = filterMat*dy*filterMat';
 	dy = dy.*~(sum(dy,2)~=0);
 	
-	psi = psi(~(on(valind)));
 	u = dy*psi;
 	v = -dx*psi;
 	
-	umesh = filterMatinner'*u;
+	umesh = filterMat'*u;
 	Umesh = reshape(umesh,[xsz,ysz])';
 	
-	vmesh = filterMatinner'*v;
+	vmesh = filterMat'*v;
 	Vmesh = reshape(vmesh,[xsz,ysz])';
 	
-	psimesh = filterMatinner'*psi;
+	psimesh = filterMat'*psi;
 	Psimesh = reshape(psimesh,[xsz,ysz])';
+	
+	Xmesh = Xmesh(2:end-1,2:end-1);
+	Ymesh = Ymesh(2:end-1,2:end-1);
+	Umesh = Umesh(2:end-1,2:end-1);
+	Vmesh = Vmesh(2:end-1,2:end-1);
+	Psimesh = Psimesh(2:end-1,2:end-1);
 	
 	if(toPlot == "surf")
 		figure(1)
